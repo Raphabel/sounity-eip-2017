0cd6cb77e27fe8fa0763894e223a895b7fc449e5
Alix FORNIELES <alixfornieles@Alixs-MacBook-Pro.local>
2016-12-09 15:27:08 +0100

    up to date

---
+++ Sounity/TimelineTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  Timeline.swift
+//  Sounity
+//
+//  Created by Alix FORNIELES on 08/12/2016.
+//  Copyright © 2016 Degraeve Raphaël. All rights reserved.
+//
+
+import UIKit
+
+class TimelineTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var timelineUserInfo: UILabel!
+    @IBOutlet weak var picture: UIImageView!
+    @IBOutlet weak var nickname: UILabel!
+    @IBOutlet weak var date: UILabel!
+    
+}
